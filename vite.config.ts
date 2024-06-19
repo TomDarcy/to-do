af9856edd349025c0e3c58c14f7febffec9956e5
Tom Darcy <tdarcytom@googlemail.com>
2024-06-19 20:45:23 +0100

    ts fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      manifest: {
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'An example of a Progressive Web App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: '/images/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
